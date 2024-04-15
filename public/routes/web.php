18f081a305a5882b8d6e73ccccb0f7085f699701
nickolasbini <nickolasbini@hotmail.com>
2024-04-15 20:19:58 -0300

    Removing debug

--- public/routes/web.php
+++ public/routes/web.php
@@ -4,8 +4,5 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    ini_set('max_execution_time', -1);
-    $result = Artisan::call('migrate:fresh');
-    dd($result);
     return view('welcome');
 });
