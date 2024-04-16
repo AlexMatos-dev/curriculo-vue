abc561e6f853bd7ab311fb499c243b1193780dac
nickolasbini <nickolasbini@hotmail.com>
2024-04-16 19:50:29 -0300

    Typo

--- public/routes/web.php
+++ public/routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-use App\Models\User;
-use Database\Seeders\CountrySeeder;
-use Database\Seeders\GenderSeeder;
-use Database\Seeders\LanguageSeeder;
-use Database\Seeders\RoleSeeder;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
