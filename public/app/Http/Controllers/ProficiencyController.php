ea512995d33cacbc637dd946bc0953c4723c7d8d
nickolasbini <nickolasbini@hotmail.com>
2024-05-20 11:34:40 -0300

    Validating input for Proficiency list method

--- public/app/Http/Controllers/ProficiencyController.php
+++ public/app/Http/Controllers/ProficiencyController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Validator;
 use App\Models\Proficiency;
 use Illuminate\Http\Request;
 
 class ProficiencyController extends Controller
 {
+    /**
+     * List all proficiency
+     * @param String category
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index(Request $request)
     {
+        Validator::validateParameters($request, [
+            'category' => 'in:'.implode(',', [Proficiency::CATEGORY_LANGUAGE, Proficiency::CATEGORY_LEVEL, Proficiency::CATEGORY_SENIORITY])
+        ]);
         $categoryName = $request->query('category', '');
         $proficiencies = Proficiency::leftJoin('translations', function($join){
             $join->on('proficiency.proficiency_level', '=', 'translations.en');
