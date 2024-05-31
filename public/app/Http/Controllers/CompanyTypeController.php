6c12d3c2abb39d8fdc75324795e54c79f135bfb3
nickolasbini <nickolasbini@hotmail.com>
2024-05-31 11:36:50 -0300

    Validating company_type method and adding some filter parameters for method

--- public/app/Http/Controllers/CompanyTypeController.php
+++ public/app/Http/Controllers/CompanyTypeController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\CompanyType;
+use App\Models\Translation;
 use Illuminate\Http\Request;
 
 class CompanyTypeController extends Controller
 {
     /**
      * List all company types
-     * @param String company types
+     * @param String name
+     * @param String languageISO - One of the official languages, default is EN
      * @return \Illuminate\Http\JsonResponse 
      */
     public function getCompanyTypes()
@@ -26,7 +28,12 @@ class CompanyTypeController extends Controller
             'company_types.created_at',
             'company_types.updated_at'
         );
-
+        if(request('name')){
+            $languageISO = request('languageISO', Translation::OFFICIAL_LANGUAGES[0]);
+            if(!in_array($languageISO, Translation::OFFICIAL_LANGUAGES))
+                $languageISO = Translation::OFFICIAL_LANGUAGES[0];
+            $companyTypes->where($languageISO, 'like', '%'.request('name').'%');
+        }
         return response()->json($companyTypes->get(), 200);
     }
 }
