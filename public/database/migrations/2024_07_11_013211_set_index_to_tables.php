2019327d70b575b1bca7fa81c066c0569b390ca1
nickolasbini <nickolasbini@hotmail.com>
2024-07-18 20:03:17 -0300

    JobList improvement at speed

--- public/database/migrations/2024_07_11_013211_set_index_to_tables.php
+++ public/database/migrations/2024_07_11_013211_set_index_to_tables.php
@@ -18,9 +18,8 @@ return new class extends Migration
          */
         Schema::table('jobslist', function (Blueprint $table) {
             $table->index('job_id');
-        });
-        Schema::table('jobslist', function (Blueprint $table) {
             $table->index('created_at');
+            $table->index('company_id');
         });
          /**
           * END of Jobslist
@@ -28,6 +27,7 @@ return new class extends Migration
 
         Schema::table('companies', function (Blueprint $table) {
             $table->index('paying');
+            $table->index('company_id');
         });
 
         Schema::table('tags', function (Blueprint $table) {
@@ -40,14 +40,17 @@ return new class extends Migration
 
         Schema::table('listlangues', function (Blueprint $table) {
             $table->index('llangue_id');
+            $table->index('llangue_name');
         });
 
         Schema::table('proficiency', function (Blueprint $table) {
             $table->index('proficiency_id');
+            $table->index('proficiency_level');
         });
 
         Schema::table('type_visas', function (Blueprint $table) {
             $table->index('typevisas_id');
+            $table->index('type_name');
         });
 
         Schema::table('translations', function (Blueprint $table) {
@@ -55,22 +58,27 @@ return new class extends Migration
         });
 
         Schema::table('job_contracts', function (Blueprint $table) {
+            $table->index('job_contract');
             $table->index('name');
         });
 
         Schema::table('job_periods', function (Blueprint $table) {
+            $table->index('job_period');
             $table->index('name');
         });
 
         Schema::table('working_visas', function (Blueprint $table) {
+            $table->index('working_visa');
             $table->index('name');
         });
 
         Schema::table('job_modalities', function (Blueprint $table) {
+            $table->index('job_modality_id');
             $table->index('name');
         });
 
         Schema::table('listcountries', function (Blueprint $table) {
+            $table->index('lcountry_id');
             $table->index('lcountry_name');
         });
 
@@ -79,7 +87,23 @@ return new class extends Migration
         });
 
         Schema::table('listprofessions', function (Blueprint $table) {
+            $table->index('lprofession_id');
             $table->index('profession_name');
+        });
+
+        Schema::table('common_currencies', function (Blueprint $table) {
+            $table->index('common_currency_id');
+            $table->index('currency');
+        });
+
+        Schema::table('company_types', function (Blueprint $table) {
+            $table->index('company_type_id');
+            $table->index('name');
+        });
+
+        Schema::table('job_payment_types', function (Blueprint $table) {
+            $table->index('job_payment_type');
+            $table->index('name');
         });
     }
 
@@ -95,8 +119,6 @@ return new class extends Migration
          */
         Schema::table('jobslist', function (Blueprint $table) {
             $table->dropIndex(['job_id']);
-        });
-        Schema::table('jobslist', function (Blueprint $table) {
             $table->dropIndex(['created_at']);
         });
          /**
@@ -117,14 +139,17 @@ return new class extends Migration
 
         Schema::table('listlangues', function (Blueprint $table) {
             $table->dropIndex(['llangue_id']);
+            $table->dropIndex(['llangue_name']);
         });
 
         Schema::table('proficiency', function (Blueprint $table) {
             $table->dropIndex(['proficiency_id']);
+            $table->dropIndex(['proficiency_level']);
         });
 
         Schema::table('type_visas', function (Blueprint $table) {
             $table->dropIndex(['typevisas_id']);
+            $table->dropIndex(['type_name']);
         });
 
         Schema::table('translations', function (Blueprint $table) {
@@ -132,22 +157,27 @@ return new class extends Migration
         });
 
         Schema::table('job_contracts', function (Blueprint $table) {
+            $table->dropIndex(['job_contract']);
             $table->dropIndex(['name']);
         });
 
         Schema::table('job_periods', function (Blueprint $table) {
+            $table->dropIndex(['job_period']);
             $table->dropIndex(['name']);
         });
 
         Schema::table('working_visas', function (Blueprint $table) {
+            $table->dropIndex(['working_visa']);
             $table->dropIndex(['name']);
         });
 
         Schema::table('job_modalities', function (Blueprint $table) {
             $table->dropIndex(['name']);
+            $table->dropIndex(['job_modality_id']);
         });
 
         Schema::table('listcountries', function (Blueprint $table) {
+            $table->dropIndex(['lcountry_id']);
             $table->dropIndex(['lcountry_name']);
         });
         
@@ -156,7 +186,23 @@ return new class extends Migration
         });
 
         Schema::table('listprofessions', function (Blueprint $table) {
+            $table->dropIndex(['lprofession_id']);
             $table->dropIndex(['profession_name']);
+        });
+
+        Schema::table('common_currencies', function (Blueprint $table) {
+            $table->dropIndex(['common_currency_id']);
+            $table->dropIndex(['currency']);
+        });
+
+        Schema::table('company_types', function (Blueprint $table) {
+            $table->dropIndex(['company_type_id']);
+            $table->dropIndex(['name']);
+        });
+
+        Schema::table('job_payment_types', function (Blueprint $table) {
+            $table->dropIndex(['job_payment_type']);
+            $table->dropIndex(['name']);
         });
     }
 };
