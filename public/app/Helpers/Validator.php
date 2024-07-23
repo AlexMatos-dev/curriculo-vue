2b781b91ce790444a32d87e222daaf389d80bca8
nickolasbini <nickolasbini@hotmail.com>
2024-07-23 10:40:07 -0300

    Return message for validator method

--- public/app/Helpers/Validator.php
+++ public/app/Helpers/Validator.php
@@ -204,11 +204,12 @@ class Validator
      */
     public static function throwResponse($message = '', $code = 400)
     {
-        response()->json(['message' => $message], $code, [
-            'Access-Control-Allow-Origin' => env('FRONTEND_URL'), 
-            'Access-Control-Allow-Credentials' => 'true',
-            'Content-Type' => 'application/json'
-        ])->send();
-        die();
+        returnResponse(['message' => $message], $code);
+        // response()->json(['message' => $message], $code, [
+        //     'Access-Control-Allow-Origin' => env('FRONTEND_URL'), 
+        //     'Access-Control-Allow-Credentials' => 'true',
+        //     'Content-Type' => 'application/json'
+        // ])->send();
+        // die();
     }
 }
