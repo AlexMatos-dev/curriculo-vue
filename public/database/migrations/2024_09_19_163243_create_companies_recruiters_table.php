492d0c8dd5dc355aabf95b4bd2db3d972a77c393
nickolasbini <nickolasbini@hotmail.com>
2024-09-19 21:14:19 -0300

    Recruiter invitation by company + Recruiter accept or refuse invitation methods

---
+++ public/database/migrations/2024_09_19_163243_create_companies_recruiters_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies_recruiters', function (Blueprint $table) {
+            $table->bigIncrements('company_recruiter_id', true);
+            $table->unsignedBigInteger('recruiter_id');
+            $table->unsignedBigInteger('company_id');
+            $table->string('token', 64)->nullable();
+            $table->string('status', 20)->nullable();
+            $table->timestamps();
+        });
+        Schema::table('companies_recruiters', function (Blueprint $table)
+		{
+			$table->foreign('recruiter_id')->references('recruiter_id')->on('recruiters')
+				->onDelete('restrict')
+				->onUpdate('restrict');
+            $table->foreign('company_id')->references('company_id')->on('companies')
+				->onDelete('restrict')
+				->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('companies_recruiters', function (Blueprint $table)
+		{
+			$table->dropForeign('companies_recruiters_recruiter_id_foreign');
+            $table->dropForeign('companies_recruiters_company_id_foreign');
+		});
+        Schema::dropIfExists('companies_recruiters');
+    }
+};
